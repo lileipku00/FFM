662b7284ac7588aec7cc9e4547101e27d49cd0e4
kasra-hosseini <hosseini@geophysik.uni-muenchen.de>
2014-04-04 15:12:13 +0200

    Check and modify read_statistics

--- matlab_statistics/Pdiff/read_statistics.m
+++ matlab_statistics/Pdiff/read_statistics.m
@@ -1,3 +1,15 @@
+% Simple script to plot the statistical analysis results in MATALB
+
+clear; clc;
+
+% ------------- INPUT --------------------
+interval = -6:0.5:6;
+% ----------------------------------------
+
+% Following address has the patchline.m which is used here
+addpath('~/Work/Scripts/gitHUB/UTILS/OTHERS')
+
+% First plotting GSN ones
 cd './GSN'
 load('t_shift_array_1.mat')
 load('t_shift_array_2.mat')
@@ -8,8 +20,6 @@ load('t_shift_array_6.mat')
 load('t_shift_array_7.mat')
 load('t_shift_array_8.mat')
 
-interval = [-5:0.5:5];
-
 t1 = hist(t_shift_array_1, interval);
 t2 = hist(t_shift_array_2, interval);
 t3 = hist(t_shift_array_3, interval);
@@ -18,41 +28,61 @@ t5 = hist(t_shift_array_5, interval);
 t6 = hist(t_shift_array_6, interval);
 t7 = hist(t_shift_array_7, interval);
 t8 = hist(t_shift_array_8, interval);
-plot(interval, t1)
+
+patchline(interval, t1, 'edgecolor', [0.478, 0.063, 0.894], ...
+    'linewidth', 4, 'edgealpha', 1.0)
 hold on
-plot(interval, t2)
-plot(interval, t3)
-plot(interval, t4)
-plot(interval, t5)
-plot(interval, t6)
-plot(interval, t7)
-plot(interval, t8)
+patchline(interval, t2, 'edgecolor', [0., 0., 1.0], ...
+    'linewidth', 4, 'edgealpha', 1.0)
+patchline(interval, t3, 'edgecolor', [0.043, 0.518, 0.78], ...
+    'linewidth', 4, 'edgealpha', 1.0)
+patchline(interval, t4, 'edgecolor', [0., 0.498, 0.], ...
+    'linewidth', 4, 'edgealpha', 1.0)
+patchline(interval, t5, 'edgecolor', [0.749, 0.749, 0.], ...
+    'linewidth', 4, 'edgealpha', 1.0)
+patchline(interval, t6, 'edgecolor', [0.871, 0.49, 0.894], ...
+    'linewidth', 4, 'edgealpha', 1.0)
+patchline(interval, t7, 'edgecolor', [0.847, 0.161, 0.], ...
+    'linewidth', 4, 'edgealpha', 1.0)
+patchline(interval, t8, 'edgecolor', [0.6, 0.2, 0.], ...
+    'linewidth', 4, 'edgealpha', 1.0)
 
-cd '../NO_GSN'
 
-load('t_shift_array_1.mat')
-load('t_shift_array_2.mat')
-load('t_shift_array_3.mat')
-load('t_shift_array_4.mat')
-load('t_shift_array_5.mat')
-load('t_shift_array_6.mat')
-load('t_shift_array_7.mat')
-load('t_shift_array_8.mat')
+% Second plotting NO_GSN ones
+% cd '../NO_GSN'
+% load('t_shift_array_1.mat')
+% load('t_shift_array_2.mat')
+% load('t_shift_array_3.mat')
+% load('t_shift_array_4.mat')
+% load('t_shift_array_5.mat')
+% load('t_shift_array_6.mat')
+% load('t_shift_array_7.mat')
+% load('t_shift_array_8.mat')
+% 
+% t1 = hist(t_shift_array_1, interval);
+% t2 = hist(t_shift_array_2, interval);
+% t3 = hist(t_shift_array_3, interval);
+% t4 = hist(t_shift_array_4, interval);
+% t5 = hist(t_shift_array_5, interval);
+% t6 = hist(t_shift_array_6, interval);
+% t7 = hist(t_shift_array_7, interval);
+% t8 = hist(t_shift_array_8, interval);
+% patchline(interval, t1, 'linestyle', '--', 'edgecolor', [0.478, 0.063, 0.894], ...
+%     'linewidth', 4, 'edgealpha', 0.5)
+% hold on
+% patchline(interval, t2, 'linestyle', '--', 'edgecolor', [0., 0., 1.0], ...
+%     'linewidth', 4, 'edgealpha', 0.5)
+% patchline(interval, t3, 'linestyle', '--', 'edgecolor', [0.043, 0.518, 0.78], ...
+%     'linewidth', 4, 'edgealpha', 0.5)
+% patchline(interval, t4, 'linestyle', '--', 'edgecolor', [0., 0.498, 0.], ...
+%     'linewidth', 4, 'linestyle', '--', 'edgealpha', 1.0)
+% patchline(interval, t5, 'linestyle', '--', 'edgecolor', [0.749, 0.749, 0.], ...
+%     'linewidth', 4, 'edgealpha', 0.5)
+% patchline(interval, t6, 'linestyle', '--', 'edgecolor', [0.871, 0.49, 0.894], ...
+%     'linewidth', 4, 'edgealpha', 0.5)
+% patchline(interval, t7, 'linestyle', '--', 'edgecolor', [0.847, 0.161, 0.], ...
+%     'linewidth', 4, 'edgealpha', 0.5)
+% patchline(interval, t8, 'linestyle', '--', 'edgecolor', [0.6, 0.2, 0.], ...
+%     'linewidth', 4, 'edgealpha', 0.5)
 
-t1 = hist(t_shift_array_1, interval);
-t2 = hist(t_shift_array_2, interval);
-t3 = hist(t_shift_array_3, interval);
-t4 = hist(t_shift_array_4, interval);
-t5 = hist(t_shift_array_5, interval);
-t6 = hist(t_shift_array_6, interval);
-t7 = hist(t_shift_array_7, interval);
-t8 = hist(t_shift_array_8, interval);
-plot(interval, t1, 'color', 'red')
-plot(interval, t2, 'color', 'red')
-plot(interval, t3, 'color', 'red')
-plot(interval, t4, 'color', 'red')
-plot(interval, t5, 'color', 'red')
-plot(interval, t6, 'color', 'red')
-plot(interval, t7, 'color', 'red')
-plot(interval, t8, 'color', 'red')
-
+cd '..'
