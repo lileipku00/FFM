7c4a4f2bcb5f4d5355beebde13fae99690010831
kasra-hosseini <hosseini@geophysik.uni-muenchen.de>
2014-05-29 22:26:53 +0200

    Completely adapted to generate raydata input file! it has been tested for P waves too

--- INVERSION_STEP01_COMMON_CORRECTION/inp_file_generator.py
+++ INVERSION_STEP01_COMMON_CORRECTION/inp_file_generator.py
@@ -15,37 +15,50 @@
 
 # Required Python modules will be imported in this part.
 import numpy as np
+import datetime
 
 import output_reader as outread
 
 """
 TODO
-- read event information from in.yspec.source
-- event id: generate a list of all events and give an id to them, here use those ids
-- kluster? I do not know how I can set this!
 - characterization of noise?
 - check all the parameters with: /home/hosseini/Work/Scripts/FFT/UTILS/FFINVERSION/AMPLITUDES/Programs/write_raydata_input
 """
 
 # ------------------- INPUT -----------------------------
-events_dir = '/home/hosseini/Work/Scripts/gitHUB/MEASUREMENTS/Pdiff_measure_1_sec_LAMBDA_1-5_90_180'
-#events_dir = '/import/neptun-helles/hosseini/FFM_RESULTS/Pdiff_measure_1_sec_LAMBDA_1-5_90_180'
+events_dir = '/home/hosseini/Work/Scripts/gitHUB/MEASUREMENTS/P_measure_1_sec_LAMBDA_1-5_32_100'
+phase = 'P'
 req_band = 'band01'
+all_events = '0274.2009.273.a'
+#all_events = True
 
+# Input file for raydata:
+input_file_name_part = 'global'
+input_counter = 1
+twinned = 'None'
+
+############## CRITERIA ###############
 # WARNING: all the intervals are min <= ... < max
 min_depth = -10
 max_depth = 1000
 
-min_xcorr = 0.8
-max_xcorr = 2
+min_xcorr = 0.85
+max_xcorr = 1
 
-min_epi = 0
-max_epi = 181
+min_epi = 32
+max_epi = 85.01
 
 check_clip = True
-# -------------------------------------------------------
+#######################################
 
-passed_event_adds = outread.event_filter(events_dir, min_dp=min_depth, max_dp=max_depth)
+bg_model = 'IASP91'
+selected_events_add = './info/selected_events_indexed.txt'
+# -------------------------------------------------------
+t_start = datetime.datetime.now()
+
+input_file_name = '%s_%s_%s' % (input_file_name_part, req_band, input_counter)
+passed_event_adds = outread.event_filter(events_dir, selected_events_add=selected_events_add, all_events=all_events,
+                                         min_dp=min_depth, max_dp=max_depth)
 
 output_files = []
 for i in range(len(passed_event_adds)):
@@ -60,18 +73,40 @@ print 'Used events  : %s' % len(output_files)
 print 'Missed events: %s' % (len(passed_event_adds) - len(output_files))
 print '========================='
 
-#passed_ev_stas = []
 passed_ev_stas_array = np.array([])
 for i in range(len(output_files)):
-    #passed_ev_sta_single = outread.station_filter(output_files[i], min_xcorr=min_xcorr, max_xcorr=max_xcorr,
-    #                                              min_epi=min_epi, max_epi=max_epi, check_clip=check_clip)
-    #if not passed_ev_sta_single.size == 0:
-    #    passed_ev_stas.append(passed_ev_sta_single)
-    #    if passed_ev_stas_array.size == 0:
-    #        passed_ev_stas_array = passed_ev_sta_single
-    #    else:
-    #        passed_ev_stas_array = np.append(passed_ev_stas_array, passed_ev_sta_single, 0)
     if passed_ev_stas_array.size == 0:
         passed_ev_stas_array = output_files[i]
     else:
         passed_ev_stas_array = np.append(passed_ev_stas_array, output_files[i], 0)
+
+filt_array = outread.array_station_filter(passed_ev_stas_array, min_xcorr=min_xcorr, max_xcorr=max_xcorr,
+                                          min_epi=min_epi, max_epi=max_epi, check_clip=check_clip)
+outread.check_selection(filt_array, min_xcorr, max_xcorr, min_epi, max_epi)
+outread.raydata_input_generator(filt_array=filt_array, input_file_name=input_file_name, twinned=twinned, phase=phase,
+                                min_xcorr=min_xcorr, min_depth=min_depth, max_depth=max_depth, min_epi=min_epi,
+                                max_epi=max_epi, check_clip=check_clip)
+outread.raydata_input(bg_model=bg_model, input_file_name=input_file_name, phase=phase)
+
+print '#event-station pairs: %s' % len(filt_array)
+print 'REGULAR END --- %s sec' % (datetime.datetime.now() - t_start)
+
+
+
+
+#### ------------------------------- TRASH ----------------------------
+####passed_ev_stas = []
+###passed_ev_stas_array = np.array([])
+###for i in range(len(output_files)):
+###    #passed_ev_sta_single = outread.station_filter(output_files[i], min_xcorr=min_xcorr, max_xcorr=max_xcorr,
+###    #                                              min_epi=min_epi, max_epi=max_epi, check_clip=check_clip)
+###    #if not passed_ev_sta_single.size == 0:
+###    #    passed_ev_stas.append(passed_ev_sta_single)
+###    #    if passed_ev_stas_array.size == 0:
+###    #        passed_ev_stas_array = passed_ev_sta_single
+###    #    else:
+###    #        passed_ev_stas_array = np.append(passed_ev_stas_array, passed_ev_sta_single, 0)
+###    if passed_ev_stas_array.size == 0:
+###        passed_ev_stas_array = output_files[i]
+###    else:
+###        passed_ev_stas_array = np.append(passed_ev_stas_array, output_files[i], 0)
--- INVERSION_STEP01_COMMON_CORRECTION/output_reader.py
+++ INVERSION_STEP01_COMMON_CORRECTION/output_reader.py
@@ -1,7 +1,9 @@
 import glob
+import matplotlib.pyplot as plt
 import numpy as np
 from obspy import UTCDateTime
 import os
+import sys
 
 ####################### output_file_reader #############################
 
@@ -36,7 +38,7 @@ def output_file_reader(evinfo, req_band='band01'):
 ####################### event_filter #############################
 
 
-def event_filter(par_add, min_dp=-10, max_dp=1000):
+def event_filter(par_add, selected_events_add, all_events=True, min_dp=-10, max_dp=1000):
     """
     Filters the events in one par_dir based on the required inputs
     """
@@ -44,19 +46,24 @@ def event_filter(par_add, min_dp=-10, max_dp=1000):
         print "%s is not a valid directory!" % par_add
         return False, False
 
-    event_adds = glob.glob(os.path.join(par_add, '*.*.*.*'))
+    selected_events = np.loadtxt(fname=selected_events_add, dtype='S', comments='#', delimiter=',')
+    event_adds = []
+    for i in range(len(selected_events[:, 1])):
+        if all_events != True:
+            if not selected_events[:, 1][i] in all_events: continue
+        event_adds.append([os.path.join(par_add, selected_events[:, 1][i]), selected_events[:, 0][i]])
     passed_event_adds = []
     for i in range(len(event_adds)):
         add_flag = False
         try:
-            fio_source = open(os.path.join(event_adds[i], 'outfiles', 'ffproc.source'), 'r')
+            fio_source = open(os.path.join(event_adds[i][0], 'outfiles', 'ffproc.source'), 'r')
             f_source = fio_source.readlines()
             ev_year, ev_julianday, ev_hr, ev_min, ev_sec, ev_msec = f_source[1].split()
             evlat, evlon, catalog_depth, inverted_depth = f_source[3].split()
             try:
                 mrr, mtt, mpp, mrt, mrp, mtp = f_source[13].split()
             except Exception, e:
-                print 'WARNING: inverted moment tensor was not found: %s' % event_adds[i]
+                print 'WARNING: inverted moment tensor was not found: %s' % event_adds[i][0]
                 mrr, mtt, mpp, mrt, mrp, mtp = f_source[7].split()
         except Exception, e:
             print 'ERROR: %s' % e
@@ -65,16 +72,144 @@ def event_filter(par_add, min_dp=-10, max_dp=1000):
         ev_date_str = ev_date_str.replace(' ', '0')
         ev_time = '%2s%2s%2s' % (ev_hr, ev_min, ev_sec)
         ev_time = ev_time.replace(' ', '0')
-        ev_id = '%s%s' % (ev_date_str, ev_time)
-
-
+        ev_id = event_adds[i][1]
 
         # Check for depth
         if min_dp <= float(inverted_depth) < max_dp:
             add_flag = True
         if add_flag:
-            passed_event_adds.append([event_adds[i], ev_date_str, ev_time, ev_id, evlat, evlon, inverted_depth])
+            passed_event_adds.append([event_adds[i][0], ev_date_str, ev_time, ev_id, evlat, evlon, inverted_depth])
     return passed_event_adds
+
+####################### array_station_filter #############################
+
+
+def array_station_filter(passed_array, min_xcorr=-100, max_xcorr=100, min_epi=0., max_epi=360., check_clip=True):
+    """
+    Filters the stations in an array based on the required inputs
+    """
+    empty_array = np.array([])
+    pass_stas_corr_1 = passed_array[passed_array[:, 6].astype(np.float) >= min_xcorr]
+
+    if not pass_stas_corr_1.size == 0:
+         pass_stas_corr_2 = pass_stas_corr_1[pass_stas_corr_1[:, 6].astype(np.float) < max_xcorr]
+    else:
+        return empty_array
+
+    passed_stas_epi_1 = pass_stas_corr_2[pass_stas_corr_2[:, 4].astype(np.float) >= min_epi]
+    if not passed_stas_epi_1.size == 0:
+        passed_stas_epi_2 = passed_stas_epi_1[passed_stas_epi_1[:, 4].astype(np.float) < max_epi]
+    else:
+        return empty_array
+    if passed_stas_epi_2.size == 0:
+        return empty_array
+
+    if check_clip:
+        passed_stas = passed_stas_epi_2[passed_stas_epi_2[:, 19].astype(np.float) < 0.1]
+
+    return passed_stas
+
+####################### check_selection #############################
+
+
+def check_selection(filt_array, min_xcorr, max_xcorr, min_epi, max_epi, check_clip=True):
+    """
+    Check whether the selection procedure worked well
+    """
+    plt.subplot(2, 2, 1)
+    plt.plot(filt_array[:, 2].astype(np.float), filt_array[:, 6].astype(np.float), 'r.', linewidth=15)
+    plt.xlabel('Latitude', size='large', weight='bold')
+    plt.ylabel('xcorr factor', size='large', weight='bold')
+    plt.xticks(size='large', weight='bold')
+    plt.yticks(size='large', weight='bold')
+    plt.hlines(min_xcorr, np.min(filt_array[:, 2].astype(np.float)), np.max(filt_array[:, 2].astype(np.float)), 'k',
+               linestyles='--')
+    plt.hlines(max_xcorr, np.min(filt_array[:, 2].astype(np.float)), np.max(filt_array[:, 2].astype(np.float)), 'k',
+               linestyles='--')
+    plt.subplot(2, 2, 2)
+    plt.plot(filt_array[:, 3].astype(np.float), filt_array[:, 6].astype(np.float), 'r.', linewidth=15)
+    plt.xlabel('Longitude', size='large', weight='bold')
+    plt.ylabel('xcorr factor', size='large', weight='bold')
+    plt.xticks(size='large', weight='bold')
+    plt.yticks(size='large', weight='bold')
+    plt.hlines(min_xcorr, np.min(filt_array[:, 3].astype(np.float)), np.max(filt_array[:, 3].astype(np.float)), 'k',
+               linestyles='--')
+    plt.hlines(max_xcorr, np.min(filt_array[:, 3].astype(np.float)), np.max(filt_array[:, 3].astype(np.float)), 'k',
+               linestyles='--')
+    plt.subplot(2, 2, 3)
+    plt.plot(filt_array[:, 4].astype(np.float), filt_array[:, 6].astype(np.float), 'b.', linewidth=15)
+    plt.xlabel('Epicentral Distance', size='large', weight='bold')
+    plt.ylabel('xcorr factor', size='large', weight='bold')
+    plt.xticks(size='large', weight='bold')
+    plt.yticks(size='large', weight='bold')
+    plt.vlines(min_epi, min_xcorr, max_xcorr, 'k', linestyles='--')
+    plt.vlines(max_epi, min_xcorr, max_xcorr, 'k', linestyles='--')
+    plt.subplot(2, 2, 4)
+    plt.plot(filt_array[:, 19].astype(np.float), filt_array[:, 6].astype(np.float), 'r.', linewidth=15)
+    plt.xlabel('Clip value', size='large', weight='bold')
+    plt.ylabel('xcorr factor', size='large', weight='bold')
+    plt.xticks(size='large', weight='bold')
+    plt.yticks(size='large', weight='bold')
+    plt.show()
+
+####################### raydata_input_generator #############################
+
+
+def raydata_input_generator(filt_array, input_file_name, twinned, phase, min_xcorr, min_depth, max_depth,
+                            min_epi, max_epi, check_clip):
+    """
+    Generate input file compatible with raydata input files
+    """
+    if not os.path.isfile(os.path.join(os.path.curdir, 'info', 'Pdef_%s' % phase)):
+        sys.exit('%s could not be found!' % os.path.join(os.path.curdir, 'info', 'Pdef_%s' % phase))
+    phase_def = open(os.path.join(os.path.curdir, 'info', 'Pdef_%s' % phase), 'r').readlines()
+    if not os.path.isfile(os.path.join(os.path.curdir, 'info', 'bpf.omega_m')):
+        sys.exit('%s could not be found!' % os.path.join(os.path.curdir, 'info', 'bpf.omega_m'))
+    filt_def = open(os.path.join(os.path.curdir, 'info', 'bpf.omega_m'), 'r').readlines()
+
+    inp_lines = []
+    inp_lines.append('%s\n' % input_file_name)
+    inp_lines.append('%s\n' % twinned)
+    inp_lines.append('# Phase : %s\n' % phase)
+    inp_lines.append('# Traveltime data, xcorr_min : %s\n' % min_xcorr)
+    inp_lines.append('# depth_min : %s, depth_max : %s\n' % (min_depth, max_depth))
+    inp_lines.append('# epi_min : %s, epi_max : %s\n' % (min_epi, max_epi))
+    inp_lines.append('# Clip : %s\n' % check_clip)
+    for ln in phase_def:
+        inp_lines.append(ln)
+    for ln in filt_def:
+        inp_lines.append(ln)
+    for sta in filt_array:
+        netid = sta[5].split('.')[0]
+        staid = sta[5].split('.')[1]
+        chaid = sta[5].split('.')[4]
+        first_line = '%s  %s  %s  %s  %s  %s  %s  %s  %s  %s  %s  %s  %s 1  0  0\n' % (sta[24], sta[25], sta[26],
+                                                                                     sta[1], staid, netid, chaid,
+                                                                                     sta[27], sta[28], sta[29],
+                                                                                     sta[2], sta[3], sta[22])
+        second_line = '1    0.0    0.0    0.0    0.0    0.0    0.0    0.0    0.0    0.0\n'
+        third_line = '      %s  %s  %s  1  %s\n' % (sta[7], sta[9], sta[6], sta[18])
+        inp_lines.append(first_line)
+        inp_lines.append(second_line)
+        inp_lines.append(third_line)
+    input_file_fio = open(input_file_name, 'w')
+    input_file_fio.writelines(inp_lines)
+
+####################### raydata_input #############################
+
+
+def raydata_input(bg_model, input_file_name, phase):
+    """
+    make in.input_file_name for raydata
+    """
+    in_input_fio = open('in.%s' % input_file_name, 'w')
+    in_input_fio.write('%s\n' % bg_model)
+    in_input_fio.write('1  20\n')
+    in_input_fio.write('%s\n' % input_file_name)
+    in_input_fio.write('Pdef_%s' % phase)
+    in_input_fio.close()
+
+
 
 ####################### station_filter #############################
 
